be4cd5063c2028d1c9b90c55c00f17d86631d12d
LeeJaewon <dlwodnjs0727@hecto.co.kr>
2026-04-16 14:37:14 +0900

    Change version to 0.0.1 and fix workflow

--- lmbridge/build.gradle.kts
+++ lmbridge/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.isroot"
                 artifactId = "lmbridge"
-                version = "1.0.0"
+                version = "0.0.1"
 
                 from(components["release"])
 
