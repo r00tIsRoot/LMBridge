b6847f3bdc535d4838fbff8f181992961b24de56
LeeJaewon <dlwodnjs0727@hecto.co.kr>
2026-04-16 14:23:57 +0900

    Add Maven publishing configuration and GitHub Actions workflow

--- lmbridge/build.gradle.kts
+++ lmbridge/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    id("maven-publish")
 }
 
 android {
@@ -40,4 +41,56 @@ dependencies {
 
     // Google LiteRT-LM dependency - fixed version 0.10.0
     implementation("com.google.ai.edge.litertlm:litertlm-android:0.10.0")
+}
+
+// Publishing configuration
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.isroot"
+                artifactId = "lmbridge"
+                version = "1.0.0"
+
+                from(components["release"])
+
+                // POM 정보
+                pom {
+                    name.set("LMBridge")
+                    description.set("Google LiteRT-LM Android SDK for on-device LLM inference")
+                    url.set("https://github.com/r00tIsRoot/LMBridge")
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("r00tIsRoot")
+                            name.set("r00tIsRoot")
+                            email.set("dlwodnjs0727@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/r00tIsRoot/LMBridge.git")
+                        developerConnection.set("scm:git:https://github.com/r00tIsRoot/LMBridge.git")
+                        url.set("https://github.com/r00tIsRoot/LMBridge")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "local"
+                url = uri("$buildDir/repo")
+            }
+            // GitHub Pages에 배포하려면 주석 해제
+            // maven {
+            //     name = "GitHubPages"
+            //     url = uri("https://r00tisroot.github.io/packages/")
+            // }
+        }
+    }
 }
