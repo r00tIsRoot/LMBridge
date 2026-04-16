ff2a79d728ca8a0d8d7398d1faa1e0964eca2e5d
LeeJaewon <dlwodnjs0727@hecto.co.kr>
2026-04-16 15:46:24 +0900

    Version bump to 0.0.5

--- lmbridge/build.gradle.kts
+++ lmbridge/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.isroot"
                 artifactId = "lmbridge"
-                version = "0.0.4"
+                version = "0.0.5"
 
                 from(components["release"])
 
