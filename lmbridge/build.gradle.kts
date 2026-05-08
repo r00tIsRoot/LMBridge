aa7b28f29fcc65615dcf5cf2d920953303ab63cd
LeeJaewon <dlwodnjs0727@hecto.co.kr>
2026-05-08 09:51:02 +0900

    [chore] 버전명 변경 v0.0.44

--- lmbridge/build.gradle.kts
+++ lmbridge/build.gradle.kts
@@ -61,7 +61,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.isroot"
                 artifactId = "lmbridge"
-                version = "0.0.43"
+                version = "0.0.44"
 
                 from(components["release"])
 
