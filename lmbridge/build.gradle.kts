f9f2002eda4c554edb7399b40e73676ef128e05c
LeeJaewon <dlwodnjs0727@hecto.co.kr>
2026-07-29 10:12:01 +0900

    Revert "[chore] 버전명 변경 v0.2.2"
    This reverts commit 2edb1f4bebdc5d5043343e33521489dfc64edc0f.

--- lmbridge/build.gradle.kts
+++ lmbridge/build.gradle.kts
@@ -61,7 +61,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.isroot"
                 artifactId = "lmbridge"
-                version = "0.2.3"
+                version = "0.2.2"
 
                 from(components["release"])
 
